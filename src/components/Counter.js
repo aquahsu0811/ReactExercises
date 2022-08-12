a43d568597527cd5e694de701d4629cb659a7fc8
aquaHsu <bellems811@gmail.com>
2022-08-12 18:30:55 +0800

    update final

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,26 @@
 
 import classes from './Counter.module.css';
-
+import { counterActions } from '../store/counter';
 import {useSelector, useDispatch} from 'react-redux';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const showCounter = useSelector(state => state.showCounter);
+  const counter = useSelector(state => state.counter.counter);
+  const showCounter = useSelector(state => state.counter.showCounter);
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle'});
+    dispatch(counterActions.toggle());
   };
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment'});
+    dispatch(counterActions.increment());
   }
 
   const decrementHandler = () =>{
-    dispatch({ type:'decrement'});
+    dispatch(counterActions.decrement());
   }
 
   const increaseHandler = () =>{
-    dispatch({ type:'increase', amount: 5});
+    dispatch(counterActions.increase(10)); //{type: SOME_UNIQUE_IDENTIFIER, payload:10}
   }
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,15 @@
-import { createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './auth';
+import counterReducer from './counter';
 
-const initState ={counter:0 , showCounter:true};
 
-const counterReducer = (state = initState, action)=>{
-    if(action.type === 'increment'){
-        return{
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type === 'decrement'){
-        return{
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type === 'increase'){
-        return{
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type === 'toggle'){
-        return{
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-
-    return state;
-}
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer:
+  {
+    counter: counterReducer,
+    auth: authReducer //{ counter: counterSlice.reducer}
+  },
+})
 
 
 export default store;
