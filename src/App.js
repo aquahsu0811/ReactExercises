aa97de22115a049704ab189cff31ee5c90a9a0de
aquahsu0811 <bellems811@gmail.com>
2022-08-19 21:45:51 +0800

    finish-add fetchCartData

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+
+import {sendCartData, fetchCartData} from './store/cart-actions';
 
 let isInitial = true;
 
@@ -14,51 +15,58 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state)=> state.ui.notification);
-  
+
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(()=>{
-    const sendCartDate = async () =>{
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title:'Sending...',
-          message:'Sending cart data!',
-        })
-      );
-      const response = await fetch('https://reacthttp-3339f-default-rtdb.firebaseio.com/cart.json',{
-        method:'PUT',
-        body:JSON.stringify(cart),
-      });
+    // const sendCartDate = async () =>{
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'pending',
+    //       title:'Sending...',
+    //       message:'Sending cart data!',
+    //     })
+    //   );
+    //   const response = await fetch('https://reacthttp-3339f-default-rtdb.firebaseio.com/cart.json',{
+    //     method:'PUT',
+    //     body:JSON.stringify(cart),
+    //   });
 
-      if(!response.ok){
-        throw new Error('Sending cart data failed.');
-      }
+    //   if(!response.ok){
+    //     throw new Error('Sending cart data failed.');
+    //   }
       
-      //const responseData = await response.json();
+    //   //const responseData = await response.json();
 
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title:'Success!',
-          message:'Sending cart data successfully!',
-        })
-      );
-    };
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'success',
+    //       title:'Success!',
+    //       message:'Sending cart data successfully!',
+    //     })
+    //   );
+    // };
 
     if(isInitial){
       isInitial = false;
       return;
     }
 
-    sendCartDate().catch(error=>{
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title:'Error!',
-          message:'Sending cart data failed!',
-        })
-      );
-    });
+    // sendCartDate().catch(error=>{
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'error',
+    //       title:'Error!',
+    //       message:'Sending cart data failed!',
+    //     })
+    //   );
+    // });
+    if(cart.changed){
+      dispatch(sendCartData(cart));
+    };
 
   },[cart, dispatch]); //dispatch nerver change, it only because include in
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -5,12 +5,18 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -26,16 +32,19 @@ const cartSlice = createSlice({
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
+      state.changed = true;
       const existingItem = state.items.find(item => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
+
 
 export const cartActions = cartSlice.actions;
 
